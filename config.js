1957a35c9a20f4776e8104735bbccc95736d56fc
nguyenamtruong18 <140128685+nguyenamtruong18@users.noreply.github.com>
2023-10-20 12:46:23 +0700

    Add files via upload
    card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Bà và Mẹ';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng Bà và Mẹ';
+const contentCard = 'Bà và Mẹ đã nhận được món quà con gửi chưa ạ? Con làm nó để dành tặng người phụ nữ con yêu thương nhất cuộc đời! Chúc bà và mẹ 20/10 vui vẻ!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = '100.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
